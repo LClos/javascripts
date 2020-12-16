7bcfed2fbbade8ded79aa60ca23297841a72b7b8
drlclos <41241576+keldoric@users.noreply.github.com>
2020-12-16 01:09:38 -0400

    Band names (#2)
    * Initial migrate - I'll move other files later
    
    * migrated band names from old VC

---
+++ band_names.js
@@ -0,0 +1,106 @@
+array1 = new Array("Jumping", "Steamy", "Nine-Fingered", "Happy", "Crazy", "Sad", "Gleeful", "Slothful", "Hideous",
+"Arrogant", "Disturbing", "Republican", "Democrat", "Depraved", "Unholy", "Stupid", "Lisping", "Swashbuckling",
+"Ponderous", "Stomping", "Dancing", "Holy", "Blessed", "Discordant", "Spastic", "Decaying", "Swaying", "Hunchbacked",
+"Doomed", "Weeping", "Smoldering", "Particular", "Untouchable", "Screaming", "Shrieking", "Sizzling", "Huge",
+"Flailing", "Moist", "Dripping", "Unpleasant", "Awful", "M.C.", "Frightening", "Drooling", "Monstrous", "Headless",
+"Three-Legged", "Sobbing", "Disadvantaged", "Really Odd", "Unrelated", "Rabid", "Carnivorous", "Flying", "Tacky",
+"Sticky", "Slippery", "Gruesome", "Horrid", "Fashionably Late", "Pregnant", "Oozing", "Dead", "Black", "White", "Green",
+"Red", "Blue", "Purple", "Pink", "Sleeping", "Comatose", "Wistful", "Deformed", "Abrasive", "Crucified", "Melting",
+"Glazed", "Blind", "Tone-Deaf", "Flayed", "Religious", "Squeaking", "Lazy-Eyed", "Teasing", "Ugly", "Smooth",
+"Prescription-Strength", "Cigar-Shaped", "Bloated", "Skanky", "Proscribed", "Vibrating", "Explosive", "Whispering",
+"Abrasive", "Sneaky", "Loathsome", "Grappling", "Groggy", "Frumpy", "Squinting", "Gargantuan", "Dim", "Hirsute",
+"Reflective", "Rock-Hard", "T-Shaped", "Voracious", "Hungarian", "Windblown", "Pockmarked", "Lardlike", "Sweaty",
+"Lollygagging");
+
+array2 = new Array("Account", "Act", "Adjustment", "Advertisement", "Agreement", "Air", "Amount", "Amusement",
+"Animal", "Answer", "Apparatus", "Approval", "Argument", "Art", "Attack", "Attempt", "Attention", "Attraction",
+"Authority", "Back", "Balance", "Base", "Behavior", "Belief", "Birth", "Bit", "Bite", "Blood", "Blow", "Body",
+"Brass", "Bread", "Breath", "Brother", "Building", "Burn", "Burst", "Business", "Butter", "Canvas", "Care", "Cause",
+"Chalk", "Chance", "Change", "Cloth", "Coal", "Color", "Comfort", "Committee", "Company", "Comparison", "Competition",
+"Condition", "Connection", "Control", "Cook", "Copper", "Copy", "Cork", "Copy", "Cough", "Country", "Cover", "Crack",
+"Credit", "Crime", "Crush", "Cry", "Current", "Curve", "Damage", "Danger", "Daughter", "Day", "Death", "Debt",
+"Decision", "Degree", "Design", "Desire", "Destruction", "Detail", "Development", "Digestion", "Direction",
+"Discovery", "Discussion", "Disease", "Disgust", "Distance", "Distribution", "Division", "Doubt", "Drink", "Driving",
+"Dust", "Earth", "Edge", "Education", "Effect", "End", "Error", "Event", "Example", "Exchange", "Existence",
+"Expansion", "Experience", "Expert", "Fact", "Fall", "Family", "Father", "Fear", "Feeling", "Fiction", "Field",
+"Fight", "Fire", "Flame", "Flight", "Flower", "Fold", "Food", "Force", "Form", "Friend", "Front", "Fruit", "Glass",
+"Gold", "Government", "Grain", "Grass", "Grip", "Group", "Growth", "Guide", "Harbor", "Harmony", "Hate", "Hearing",
+"Heat", "Help", "History", "Hole", "Hope", "Hour", "Humor", "Ice", "Idea", "Impulse", "Increase", "Industry", "Ink",
+"Insect", "Instrument", "Insurance", "Interest", "Invention", "Iron", "Jelly", "Join", "Journey", "Judge", "Jump",
+"Kick", "Kiss", "Knowledge", "Land", "Language", "Laugh", "Low", "Lead", "Learning", "Leather", "Letter", "Level",
+"Lift", "Light", "Limit", "Linen", "Liquid", "List", "Look", "Loss", "Love", "Machine", "Man", "Manager", "Mark",
+"Market", "Mass", "Meal", "Measure", "Meat", "Meeting", "Memory", "Metal", "Middle", "Milk", "Mind", "Mine",
+"Minute", "Mist", "Money", "Month", "Morning", "Mother", "Motion", "Mountain", "Move", "Music", "Name", "Nation",
+"Need", "News", "Night", "Noise", "Note", "Number", "Observation", "Offer", "Oil", "Operation", "Opinion", "Order",
+"Organization", "Ornament", "Owner", "Page", "Pain", "Paint", "Paper", "Part", "Paste", "Payment", "Peace", "Person",
+"Place", "Plant", "Play", "Pleasure", "Point", "Poison", "Polish", "Porter", "Position", "Powder", "Power", "Price",
+"Print", "Process", "Produce", "Profit", "Property", "Prose", "Protest", "Pull", "Punishment", "Purpose", "Push",
+"Quality", "Question", "Rain", "Range", "Rate", "Ray", "Reaction", "Reading", "Reason", "Record", "Regret", "Relation",
+"Religion", "Representative", "Request", "Respect", "Rest", "Reward", "Rhythm", "Rice", "River", "Road", "Roll",
+"Room", "Rub", "Rule", "Run", "Salt", "Sand", "Scale", "Science", "Sea", "Seat", "Secretary", "Selection",
+"Self", "Sense", "Servant", "Sex", "Shade", "Shake", "Shame", "Shock", "Side", "Sign", "Silk", "Silver", "Sister",
+"Size", "Sky", "Sleep", "Slip", "Slope", "Smash", "Smell", "Smile", "Smoke", "Sneeze", "Snow", "Soap", "Society",
+"Son", "Song", "Sort", "Sound", "Soup", "Space", "Stage", "Start", "Statement", "Steam", "Steel", "Step", "Stitch",
+"Stone", "Stop", "Story", "Stretch", "Structure", "Substance", "Sugar", "Suggestion", "Summer", "Support", "Surprise",
+"Swim", "System", "Talk", "Taste", "Tax", "Teaching", "Tendency", "Test", "Theory", "Thing", "Thought", "Thunder",
+"Time", "Tin", "Top", "Touch", "Trade", "Transport", "Trick", "Trouble", "Turn", "Twist", "Unit", "Use", "Value",
+"Verse", "Vessel", "View", "Voice", "Walk", "War", "Wash", "Waste", "Water", "Wave", "Wax", "Way", "Weather", "Week",
+"Weight", "Wind", "Wine", "Winter", "Woman", "Wood", "Wool", "Word", "Work", "Wound", "Writing", "Year", "Nouns",
+"Angle", "Ant", "Apple", "Arch", "Arm", "Army", "Baby", "Bag", "Ball", "Band", "Basin", "Basket", "Bath", "Bed",
+"Bee", "Bell", "Berry", "Bird", "Blade", "Board", "Boat", "Bone", "Book", "Boot", "Bottle", "Box", "Boy", "Brain",
+"Brake", "Branch", "Brick", "Bridge", "Brush", "Bucket", "Bulb", "Button", "Cake", "Camera", "Card", "Carriage",
+"Cart", "Cat", "Chain", "Cheese", "Chess", "Chin", "Church", "Circle", "Clock", "Cloud", "Coat", "Collar", "Comb",
+"Cord", "Cow", "Cup", "Curtain", "Cushion", "Dog", "Door", "Drain", "Drawer", "Dress", "Drop", "Ear", "Egg", "Engine",
+"Eye", "Face", "Farm", "Feather", "Finger", "Fish", "Flag", "Floor", "Fly", "Foot", "Fork", "Fowl", "Frame", "Garden",
+"Girl", "Glove", "Goat", "Gun", "Hair", "Hammer", "Hand", "Hat", "Head", "Heart", "Hook", "Horn", "Horse", "Hospital",
+"House", "Island", "Jewel", "Kettle", "Key", "Knee", "Knife", "Knot", "Leaf", "Leg", "Library", "Line", "Lip", "Lock",
+"Map", "Match", "Monkey", "Moon", "Mouth", "Muscle", "Nail", "Neck", "Needle", "Nerve", "Net", "Nose", "Nut", "Office",
+"Orange", "Oven", "Parcel", "Pen", "Pencil", "Picture", "Pig", "Pin", "Pipe", "Plane", "Plate", "Plough", "Pocket",
+"Pot", "Potato", "Prison", "Pump", "Rail", "Rat", "Receipt", "Ring", "Rod", "Roof", "Root", "Sail", "School",
+"Scissors", "Screw", "Seed", "Sheep", "Shelf", "Ship", "Shirt", "Shoe", "Skin", "Skirt", "Snake", "Sock", "Spade",
+"Sponge", "Spoon", "Spring", "Square", "Stamp", "Star", "Station", "Stem", "Stick", "Stocking", "Stomach", "Store",
+"Street", "Sun", "Table", "Tail", "Thread", "Throat", "Thumb", "Ticket", "Toe", "Tongue", "Tooth", "Town", "Train",
+"Tray", "Tree", "Trousers", "Umbrella", "Wall", "Watch", "Wheel", "Whip", "Whistle", "Window", "Wing", "Wire");
+
+array3 = new Array("magedon", "mastery", "storm", "scape", "ship", "age", "ness", "")
+
+array4 = new Array("from Uranus", "of Doom", "of Unholy Terror", "of Satan", "from Hell", "in Love", "of Ill Will",
+"of Pain", "in Agony", "Eating Pudding",  "Burning Alive", "Dripping Blood", "Swaying Up the Stairs",
+"Looking For Love", "Being Naughty", "Committing Crimes", "Seeking Punishment", "Posing Nude", "Playing With Mud",
+"Laying Logs", "Pulling Taffy", "Winning Trophies", "Whining Loudly", "Eating Pork", "Chewing Gum", "in the Backyard",
+"in the Basement", "Just Outside the Door", "Right Behind You", "Selling Lies", "Busting Rhymes", "Acting Up",
+"Playing Innocent", "A La Mode", "Thinking Dark Thoughts", "On Welfare", "On A Stick", "On A Cross", "On A Rope",
+"in Chains", "Bleating Loudly", "Greasing the Noodle", "After Revenge", "Out For Blood", "Fighting Goodness",
+"Eating Dwarves", "Rappin' Poorly", "of Hopelessness", "Gloating Endlessly", "of Effervescent Therapy",
+"of Lordlike Proportions", "of Heavenly Might", "with Gravy Topping", "served Fresh", "Rotting in the Sun",
+"Erupting Needlessly", "Torn Between Two Lovers", "Removed Forcefully", "in the House", "Exuding Confidence");
+
+function getBandName()
+{
+    to_return = ""
+    if (Math.random() > 0.5)
+    {
+        index1 = Math.round(Math.random() * array1.length);
+        to_return += array1[index1] + " "
+    }
+	index2 = Math.round(Math.random() * array2.length);
+    to_return += array2[index2]
+    if (Math.random() > 0.98)
+    {
+        index3 = Math.round(Math.random() * array3.length);
+        to_return += array3[index3]
+    }
+    to_return += " "
+    if (Math.random() > 0.8)
+    {
+        index2 = Math.round(Math.random() * array2.length);
+        to_return += array2[index2] + " "
+    }
+    if (Math.random() > 0.8)
+    {
+        index4 = Math.round(Math.random() * array4.length);
+        to_return += array4[index4] + " "
+    }
+
+	return to_return
+}
